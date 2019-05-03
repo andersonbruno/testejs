dd546cd0f8aa840217408a4839b4040b1f6b6be5
andersonbruno <bruno-squall@hotmail.com>
2019-05-03 13:01:24 -0300

    RUles

---
+++ rules.js
@@ -0,0 +1,12 @@
+{
+	"name": "Ansu rules",
+	"author": "Ansu",
+	"description": "An example of a GitHub remote Rule to be imported",
+
+	"selector": "google",
+	"onLoad": true,
+
+	"code": {
+		"js": "js/file.js"
+	}
+}
